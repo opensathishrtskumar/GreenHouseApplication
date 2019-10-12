d7d883c37f6bf73e9e36de4aecf172e3fd7e401a
Sathish Kumar <sathishrtskumar@gmail.com>
2019-10-12 13:11:07 +0530

    Removed unused DAO

--- src/main/java/org/lemma/ems/base/dao/PollingDetailsDAO.java
+++ src/main/java/org/lemma/ems/base/dao/PollingDetailsDAO.java
@@ -116,8 +116,8 @@ public class PollingDetailsDAO {
 			public SettingsDTO mapRow(ResultSet rs, int rowNum) throws SQLException {
 				SettingsDTO dto = new SettingsDTO();
 				dto.setId(rs.getLong("id"));
-				dto.setConfigName(rs.getString("configname"));
-				dto.setConfigValue(rs.getString("configvalue"));
+				/*dto.setConfigName(rs.getString("configname"));
+				dto.setConfigValue(rs.getString("configvalue"));*/
 
 				return dto;
 			}
